d809f621e406c4631e68a5ce8c84fa84d6605319
balasubram <balasubramanian.j.s@gmail.com>
2019-04-14 09:22:29 +0800

    Used VCAP_SERVICES

--- server/routers/transaction.js
+++ server/routers/transaction.js
@@ -6,13 +6,8 @@ const ibmdb = require("ibm_db");
 module.exports = function (app) {
   const router = express.Router();
 
-  const db2 = {
-        host: 'dashdb-txn-sbox-yp-dal09-03.services.dal.bluemix.net',
-        port: 50000,
-        username: 'pnw62297',
-        password: 'jmwmp^gk5d4s265g',
-        database: 'BLUDB'
-    };
+  var env = JSON.parse(process.env.VCAP_SERVICES);
+  var db2 = env['dashDB For Transactions'][0].credentials;
 
   const connString = "DRIVER={DB2};DATABASE=" + db2.database + ";UID=" + db2.username + ";PWD=" + db2.password + ";HOSTNAME=" + db2.host + ";port=" + db2.port;
 
